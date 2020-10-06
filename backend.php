0960a443b1fc61557d67dc2adcdc1dbea57c50af
ikom9120 <ikom9120@gmail.com>
2020-10-06 20:46:59 +0300

    JL-87 jQuery 3

---
+++ backend.php
@@ -0,0 +1,5 @@
+<?php
+set_time_limit(60);
+
+
+echo '{"speed10":8.5,"degree10":321,"speed20":9.4,"degree20":321,"speed30":9.6,"degree30":321,"speed40":9.9,"degree40":322,"speed50":10.1,"degree50":322,"speed80":10.8,"degree80":322,"speed100":11,"degree100":322,"t":22,"tdmax":22,"tdmin":0,"aptmp":22,"r":72,"sp":101349,"crain":0,"tstm":1,"cape":1,"prmsl":101568,"vis":24135,"cpofp":-50,"latitude":46.25,"longitude":30.5}';
